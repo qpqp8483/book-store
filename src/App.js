92f2ab7e24e478425f583b870795fbd59eed652a
chanwoo <qpqp8483@naver.com>
2022-08-03 20:40:30 +0900

    require additional App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import BookSearch from "./components/BookSearch";
 import { bookSearch } from "./KakaoApi";
 
 const App = () => {
@@ -8,13 +9,11 @@ const App = () => {
 
   useEffect(() => {
     if (queryText.length > 0) {
-      console.log(queryText);
       kakaoSearch(queryText, true); // 컴포넌트 마운트 후에, 함수를 호출한다.
     }
   }, [queryText]);
 
   const kakaoSearch = async (query, reset) => {
-    console.log(query);
     const params = {
       query: query,
       sort: "accuracy", // accuracy | recency 정확도 or 최신
@@ -30,33 +29,17 @@ const App = () => {
     }
   };
   const searchBook = (text) => {
-    console.log(text);
     setQueryText(text);
   };
 
-  // useEffect(() => {
-  //   kakaoSearch().then((res) => {
-  //     setBooks(res.data.documents); //setBooks에 res.data.documents 할당
-  //     console.log(res.data.documents); //나옴
-  //     console.log(books); //안나옴
-  //   });
-  // }, []); //의존정배열에 books넣을경우 무한루프,,,
   return (
     <div>
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+      <BookSearch
+        books={books}
+        text={text}
+        searchBook={searchBook}
+        setText={setText}
       />
-      <button onClick={() => searchBook(text)}>버튼</button>
-      {/* {JSON.stringify(books)} */}
-      {books.map((book, index) => (
-        <div key={index}>
-          {book.contents}
-          <br />
-          <br />
-        </div>
-      ))}
     </div>
   );
 };
